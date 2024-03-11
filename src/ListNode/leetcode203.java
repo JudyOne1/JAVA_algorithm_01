f6f602c4f989100758a08eb98840aa0a97f7f8aa
Judy <mzyy867734122@163.com>
2024-03-11 17:06:07 +0800

    finish 203

---
+++ src/ListNode/leetcode203.java
@@ -0,0 +1,49 @@
+package ListNode;
+
+
+public class leetcode203 {
+    public ListNode removeElements(ListNode head, int val) {
+        //空链表
+        if (head == null){
+            return head;
+        }
+
+        //执行删除
+        ListNode helper = head;
+        while (helper.next != null){
+
+            //本身是需要删除的
+            if (head.val == val && head.next != null){
+                head = head.next;
+                helper = head;
+            }else if (head.val == val && head.next == null){
+                return null;
+            }else if (helper.next.val == val){
+                //本身不需要删除，next需要删除
+                helper.next = helper.next.next;
+            }else {
+                helper = helper.next;
+            }
+            //到最后一个了
+            if (helper.next == null){
+                break;
+            }
+            //往后移动
+
+        }
+
+        //只有一个值的链表
+//        if (head.val == val){
+//            return head = null;
+//        }
+        return head;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+}
