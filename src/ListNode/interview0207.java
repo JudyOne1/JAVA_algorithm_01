47aa0136440f9db5370f0e38e78a93c07cd21730
JudyMAC <mzyy867734122@163.com>
2024-03-13 15:02:06 +0800

    transmit 0207

---
+++ src/ListNode/interview0207.java
@@ -0,0 +1,12 @@
+package ListNode;
+
+public class interview0207 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        return headA;
+    }
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) { this.val = val;this.next = null; }
+    }
+}
