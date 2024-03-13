4ef7ee99af933c62f2c939d9132710f59948e69c
JudyMAC <mzyy867734122@163.com>
2024-03-13 15:01:30 +0800

    finish 19

---
+++ src/ListNode/leetcode19.java
@@ -0,0 +1,33 @@
+package ListNode;
+
+public class leetcode19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.next = head;
+
+        ListNode fastIndex = dummyNode;
+        ListNode slowIndex = dummyNode;
+
+        // 只要快慢指针相差 n 个结点即可
+        for (int i = 0; i <= n  ; i++){
+            fastIndex = fastIndex.next;
+        }
+
+        while (fastIndex != null){
+            fastIndex = fastIndex.next;
+            slowIndex = slowIndex.next;
+        }
+
+        //此时 slowIndex 的位置就是待删除元素的前一个位置。
+        //具体情况可自己画一个链表长度为 3 的图来模拟代码来理解
+        slowIndex.next = slowIndex.next.next;
+        return dummyNode.next;
+    }
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
