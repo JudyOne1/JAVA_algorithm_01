709722c5543457a30c014377ed568120b3e59e7d
JudyMAC <mzyy867734122@163.com>
2024-03-21 18:35:55 +0800

    finish 112｜513

--- src/BinaryTree/leetcode112.java
+++ src/BinaryTree/leetcode112.java
@@ -1,4 +1,89 @@
 package BinaryTree;
 
+import java.util.Stack;
+
 public class leetcode112 {
+    /**
+     * 检查二叉树中是否存在一条从根节点到叶子节点的路径，使得路径上所有节点值的和等于给定的目标和。
+     * 使用栈模拟的前序遍历
+     * @param root 二叉树的根节点
+     * @param targetSum 目标和
+     * @return 如果存在满足条件的路径则返回true，否则返回false
+     */
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null) return false;
+        // 使用两个栈，一个存储节点，一个存储节点值的和
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        stack1.push(root);
+        stack2.push(root.val);
+        while(!stack1.isEmpty()) {
+            int size = stack1.size();
+
+            for(int i = 0; i < size; i++) {
+                TreeNode node = stack1.pop();
+                int sum = stack2.pop();
+
+                // 遇到叶子节点时判断当前路径的和是否等于目标和
+                if(node.left == null && node.right == null && sum == targetSum) {
+                    return true;
+                }
+                // 遍历右子树
+                if(node.right != null){
+                    stack1.push(node.right);
+                    stack2.push(sum + node.right.val);
+                }
+                // 遍历左子树
+                if(node.left != null) {
+                    stack1.push(node.left);
+                    stack2.push(sum + node.left.val);
+                }
+            }
+        }
+        return false;
+    //    return process(root, targetSum);
+    }
+
+
+//    public boolean process(TreeNode root, int targetSum) {
+//        //base case
+//        targetSum -= root.val;
+//        // 叶子结点
+//        if (root.left == null && root.right == null) {
+//            return targetSum == 0;
+//        }
+//        if (root.left != null) {
+//            boolean left = process(root.left, targetSum);
+//            if (left) {      // 已经找到
+//                return true;
+//            }
+//        }
+//        if (root.right != null) {
+//            boolean right = process(root.right, targetSum);
+//            if (right) {     // 已经找到
+//                return true;
+//            }
+//        }
+//        return false;
+//
+//    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
--- src/BinaryTree/leetcode513.java
+++ src/BinaryTree/leetcode513.java
@@ -1,4 +1,61 @@
 package BinaryTree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 给定一个二叉树的 根节点 root，请找出该二叉树的 最底层 最左边 节点的值。
+ *
+ * 假设二叉树中至少有一个节点。
+ *
+ * 层序遍历的最后一层的第一个元素即为该二叉树的最底层最左边节点。
+ */
 public class leetcode513 {
+    public int findBottomLeftValue(TreeNode root) {
+        if (root.left == null && root.right == null){
+            return root.val;
+        }
+        // 用于存储结果的列表
+        ArrayList<List<Integer>> resList = new ArrayList<List<Integer>>();
+        // 使用队列进行层级遍历
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root); // 将根节点入队列
+
+        // 当队列不为空时，持续进行层级遍历
+        while (!queue.isEmpty()) {
+            // 当前层的节点列表
+            List<Integer> itemList = new ArrayList<Integer>();
+            int len = queue.size(); // 当前层的节点数量
+
+            // 遍历当前层的所有节点
+            while (len > 0) {
+                TreeNode tmpNode = queue.poll(); // 取出队列中的节点
+                itemList.add(tmpNode.val); // 将节点值添加到当前层的节点列表中
+
+                // 如果节点有左孩子，则将其入队列
+                if (tmpNode.left != null) queue.offer(tmpNode.left);
+                // 如果节点有右孩子，则将其入队列
+                if (tmpNode.right != null) queue.offer(tmpNode.right);
+                len--; // 当前层节点数量减一
+            }
+
+            // 将当前层的节点列表添加到结果列表中
+            resList.add(itemList);
+        }
+        return resList.get(resList.size() - 1).get(0);
+    }
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
