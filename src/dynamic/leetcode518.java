900878903eccf796927a13757ab7d427596e4f69
Judy <mzyy867734122@163.com>
2024-04-02 18:40:19 +0800

    finish 518

--- src/dynamic/leetcode518.java
+++ src/dynamic/leetcode518.java
@@ -19,17 +19,40 @@ public class leetcode518 {
         return ways;
     }
     public int dp(int amount, int[] coins) {
-
-        int[][] dp = new int[coins.length + 1][amount + 1];
-        for (int i = 0; i < dp.length; i++) {
-            dp[i][0] = 1;
+        if (coins == null || coins.length == 0 || amount < 0) {
+            return 0;
         }
-        for (int i = 1; i < dp.length; i++) {
-            for (int j = 1; j < dp[0].length; j++) {
-                dp[i][j] += dp[i + 1][j-];
+        int N = coins.length;
+        int[][] dp = new int[N + 1][amount + 1];
+        dp[N][0] = 1;
+        for (int index = N - 1; index >= 0; index--) {
+            for (int rest = 0; rest <= amount; rest++) {
+                int ways = 0;
+
+                for (int zhang = 0; zhang * coins[index] <= rest; zhang++) {//需要枚举(for循环)才能搞出一个格子，需要继续优化
+                    ways += dp[index + 1][rest - (zhang * coins[index])];
+                }
+                dp[index][rest] = ways;
             }
         }
-        return dp[coins.length][amount];
+        return dp[0][amount];
+    }
+    public static int dp2(int amount,int[] coins) {
+        if (coins == null || coins.length == 0 || amount < 0) {
+            return 0;
+        }
+        int N = coins.length;
+        int[][] dp = new int[N + 1][amount + 1];
+        dp[N][0] = 1;
+        for (int index = N - 1; index >= 0; index--) {
+            for (int rest = 0; rest <= amount; rest++) {
+                dp[index][rest] = dp[index + 1][rest];// a 楼下位置的值
+                if (rest - coins[index] >= 0) {
+                    dp[index][rest] += dp[index][rest - coins[index]];// ※
+                }
+            }
+        }
+        return dp[0][amount];
     }
 //    public int process(int amount, int[] coins,int index){
 //        if(amount == 0){
