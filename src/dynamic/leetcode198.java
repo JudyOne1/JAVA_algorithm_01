1c5a324ac5b44073d056de74c639f3589bade120
Judy <mzyy867734122@163.com>
2024-04-04 17:50:09 +0800

    sync 198

---
+++ src/dynamic/leetcode198.java
@@ -0,0 +1,27 @@
+package dynamic;
+
+public class leetcode198 {
+    public int rob(int[] nums) {
+        return process(nums, 0);
+    }
+
+    public int process(int[] nums, int index) {
+        if (index == nums.length - 1) {
+            return nums[index];
+        }
+        if (index >= nums.length) {
+            return -1;
+        }
+        int p1 = process(nums, index + 1);//不抢当前这家 1 3
+        int p2 = 0;
+        int next = process(nums, index + 2) + nums[index];//抢当前这家 0  2
+        if (next != -1) {
+            p2 = next;
+        }
+        return Math.max(p1, p2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new leetcode198().rob(new int[]{1, 2, 3, 1}));
+    }
+}
