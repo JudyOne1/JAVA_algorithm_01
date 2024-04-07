60487d9ec4953200eed1de36d5537bfe08609153
Judy <mzyy867734122@163.com>
2024-04-07 18:47:57 +0800

    finish 1 again

--- src/hashTable/leetcode1.java
+++ src/hashTable/leetcode1.java
@@ -7,16 +7,15 @@ public class leetcode1 {
         HashMap<Integer, Integer> hashMap = new HashMap<>();
         //key 存数字，value 存下标；
         for (int i = 0; i < nums.length; i++) {
-            if (hashMap.containsKey(target-nums[i])){
+            if (hashMap.containsKey(target - nums[i])) {
                 //如果存在所需要的数字，那么就找到了，返回下标即可
-                return new int[]{hashMap.get(target-nums[i]), i};
-            }else {
+                return new int[]{hashMap.get(target - nums[i]), i};
+            } else {
                 //如果不存在，那么就将当前数字存入hashMap中，key为当前数字，value为下标
                 hashMap.put(nums[i], i);
             }
         }
         return null;
-
 
 //        for (int i = 0; i < nums.length; i++) {
 //            for (int j = i + 1; j < nums.length; j++){
@@ -27,4 +26,17 @@ public class leetcode1 {
 //        }
 //        return null;
     }
+
+    public int[] twoSum100(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(target - nums[i])){
+                return new int[]{hashMap.get(target - nums[i]), i};
+            }else{
+                hashMap.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+
 }
