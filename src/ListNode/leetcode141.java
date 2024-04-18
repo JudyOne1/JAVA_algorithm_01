8f44f1d5105e07cc4a5266684d94c8d1d49429c7
judymac <mzyy867734122@163.com>
2024-04-18 15:34:45 +0800

    sync 141

---
+++ src/ListNode/leetcode141.java
@@ -0,0 +1,16 @@
+package ListNode;
+
+public class leetcode141 {
+    public boolean hasCycle(ListNode head) {
+
+    }
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
