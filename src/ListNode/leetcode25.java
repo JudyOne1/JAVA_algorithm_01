2ccba1e198cde1ef366c4bfe71d0408822ec4bec
Judy <mzyy867734122@163.com>
2024-04-19 18:45:23 +0800

    finish 25

--- src/ListNode/leetcode25.java
+++ src/ListNode/leetcode25.java
@@ -2,12 +2,36 @@ package ListNode;
 
 public class leetcode25 {
     public ListNode reverseKGroup(ListNode head, int k) {
-        //先翻转链表，用两个临时变量记录下一个节点和上一个节点，翻转前面的指定位置的节点
+        // 统计链表节点个数
+        int count = 0;
+        for (ListNode cur = head; cur != null; cur = cur.next)
+            ++count;
+
+        // 使用虚拟头节点方便操作
+        ListNode dummy = new ListNode();
+        ListNode p0 = dummy;
         ListNode pre = null;
         ListNode cur = head;
 
+        // 遍历链表，每次处理一个长度为 k 的子列表
+        for (int i = 0; i < count / k; ++i) {
+            // 翻转子列表
+            for (int j = 0; j < k; ++j) {
+                ListNode next = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = next;
+            }
 
+            // 将翻转后的子列表连接到原链表的适当位置
+            ListNode next = p0.next;
+            p0.next.next = cur;
+            p0.next = pre;
+            p0 = next;
+        }
 
+        // 返回翻转后的链表的头节点
+        return dummy.next;
     }
 
     class ListNode {
