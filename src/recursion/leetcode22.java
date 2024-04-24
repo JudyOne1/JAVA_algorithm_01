e64bd129942075f4471a8fd34785b9eda6de9fc4
Judy <mzyy867734122@163.com>
2024-04-24 15:48:51 +0800

    finish 22

--- src/recursion/leetcode22.java
+++ src/recursion/leetcode22.java
@@ -1,9 +1,35 @@
 package recursion;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class leetcode22 {
     public List<String> generateParenthesis(int n) {
+        ArrayList<String> ans = new ArrayList<>();
+        String str = "";
+        int leftParenthesesNums = 0;
+        int index = 0;
+        dfs(n, ans, str, leftParenthesesNums, index);
+        return ans;
+    }
 
+    private void dfs(int n, ArrayList<String> ans, String str, int leftParenthesesNums, int index) {
+        // 当处理到字符串的长度等于 n * 2 时，将其加入到结果列表中
+        if (index == n * 2) {
+            ans.add(str);
+            return;
+        }
+        if (leftParenthesesNums < n) {
+            //左括号数量小于n，放左括号
+            str += "(";
+            dfs(n, ans, str, leftParenthesesNums + 1, index + 1);
+            //回溯，移除最后一个添加的左括号
+            str = str.substring(0, str.length() - 1);
+        }
+        if (index - leftParenthesesNums < leftParenthesesNums) {
+            //右括号数量小于左括号数量，放右括号
+            str += ")";
+            dfs(n, ans, str, leftParenthesesNums, index + 1);
+        }
     }
 }
