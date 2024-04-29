288ac2b7d827805a28be2fe7575325f65e959530
judymac <mzyy867734122@163.com>
2024-04-29 16:51:01 +0800

    update 121|322

--- src/dynamic/leetcode121.java
+++ src/dynamic/leetcode121.java
@@ -2,20 +2,25 @@ package dynamic;
 
 public class leetcode121 {
     public int maxProfit(int[] prices) {
+        // 初始化买入成本为最大整数，利润为0
         int cost = Integer.MAX_VALUE, profit = 0;
+        // 遍历每一天的价格
         for (int price : prices) {
+            // 更新买入成本为当前价格和之前买入成本的较小值
             cost = Math.min(cost, price);
+            // 更新利润为当前价格减去买入成本和之前利润的较大值
             profit = Math.max(profit, price - cost);
         }
         return profit;
-//        int max = 0;
-//        for (int i = 0; i < prices.length; i++) {
-//            for (int j = i + 1; j < prices.length; j++) {
-//                if (prices[j] > prices[i]) {
-//                    max = Math.max(max, prices[j] - prices[i]);
-//                }
-//            }
-//        }
-//        return max;
+// 旧算法：使用双重循环遍历所有可能的买入和卖出组合，然后计算利润
+// int max = 0;
+// for (int i = 0; i < prices.length; i++) {
+//     for (int j = i + 1; j < prices.length; j++) {
+//         if (prices[j] > prices[i]) {
+//             max = Math.max(max, prices[j] - prices[i]);
+//         }
+//     }
+// }
+// return max;
     }
 }
--- src/dynamic/leetcode322.java
+++ src/dynamic/leetcode322.java
@@ -4,6 +4,65 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class leetcode322 {
+    public int coinChange1(int[] coins, int amount) {
+        int ans = dfs(coins, amount, 0);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+
+    }
+
+    private int dp1(int[] coins, int amount) {        // 如果目标金额为0，不需要任何硬币
+        if (amount == 0) {
+            return 0;
+        }
+        int N = coins.length;
+        int[][] dp = new int[N + 1][amount + 1];
+
+        // 初始化边界条件
+        dp[N][0] = 0;
+        for (int j = 1; j <= amount; j++) {
+            dp[N][j] = Integer.MAX_VALUE; // 设为最大值表示不可达
+        }
+
+        // 从最后一个硬币开始向前计算
+        for (int index = N - 1; index >= 0; index--) {
+            for (int rest = 0; rest <= amount; rest++) {
+                int ans = Integer.MAX_VALUE; // 初始化为最大值，表示当前金额在不使用当前硬币时的最小需要硬币数
+
+                // 尝试使用当前硬币
+                for (int zhang = 0; zhang * coins[index] <= rest; zhang++) {
+                    int next = dp[index + 1][rest - zhang * coins[index]];
+                    // 如果使用当前硬币后能够达到剩余金额，则更新最小硬币数
+                    if (next != Integer.MAX_VALUE) {
+                        ans = Math.min(ans, zhang + next);
+                    }
+                }
+
+                dp[index][rest] = ans; // 记录使用当前硬币得到的最小硬币数
+            }
+        }
+
+        return dp[0][amount]; // 返回目标金额所需的最小硬币数
+    }
+
+
+    private int dfs(int[] coins, int amount, int index) {
+        if (index == coins.length) {//没钱了，且rest=0，那么只需要0张组成rest
+            return amount == 0 ? 0 : Integer.MAX_VALUE;
+        }
+
+        // 从后往前遍历硬币面额，尝试使用当前面额的硬币
+        int ans = Integer.MAX_VALUE;
+        for (int zhang = 0; zhang <= amount / coins[index]; zhang++) {
+            // 递归计算剩余金额需要的最少硬币数量
+            int next = dfs(coins, amount - zhang * coins[index], index + 1);
+            // 如果递归有解，则更新最少硬币数量
+            if (next != Integer.MAX_VALUE) {
+                ans = Math.min(ans, next + zhang);
+            }
+        }
+        return ans;
+    }
+
     public int coinChange(int[] coins, int amount) {
         if (amount == 0) {
             return 0;
