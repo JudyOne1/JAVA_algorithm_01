d3f4cfc57a846f1b77e9832d337ae1255c1f30b8
Judy <mzyy867734122@163.com>
2024-05-02 18:34:59 +0800

    finish 5|64

---
+++ src/dynamic/leetcode5.java
@@ -0,0 +1,85 @@
+package dynamic;
+
+public class leetcode5 {
+    public String longestPalindrome0(String s) {
+        // 初始化最长回文子串为空字符串
+        int len = s.length();
+        String result = "";
+
+        // 遍历所有可能的回文子串中心，包括奇数长度和偶数长度的回文子串
+        for (int i = 0; i < len * 2 - 1; i++) {
+            int left = i / 2; // 计算回文子串的左边界
+            int right = left + i % 2; // 计算回文子串的右边界
+            // 扩展回文子串
+            while (left >= 0 && right < len && s.charAt(left) == s.charAt(right)) {
+                String tmp = s.substring(left, right + 1); // 获取当前回文子串
+                // 更新最长回文子串
+                if (tmp.length() > result.length()) {
+                    result = tmp;
+                }
+                left--; // 左边界向左移动
+                right++; // 右边界向右移动
+            }
+        }
+        return result;
+    }
+    public int countSubstrings00(String s) {
+        int len = s.length();
+        //单个字符，每个字符都是一个回文串
+        int ans = len;
+        //以单个字符为中心向两侧扩散
+        for(int i = 0; i < len; i++){
+            int left = i - 1;
+            int right = i + 1;
+            while (left >= 0 && right < len && s.charAt(left) == s.charAt(right)){
+                ans++;
+                left--;
+                right++;
+            }
+        }
+        //以两个字符为中心向两侧扩散
+        for(int i = 0; i < len - 1; i++){
+            int left = i;
+            int right = i + 1;
+            while (left >= 0 && right < len && s.charAt(left) == s.charAt(right)){
+                ans++;
+                left--;
+                right++;
+            }
+        }
+        return ans;
+    }
+    public String longestPalindrome(String s) {
+        int length = s.length();
+        int maxStart = 0; // 最长回文子串的起始位置
+        int maxLength = 0; // 最长回文子串的长度
+
+        // 遍历字符串中每个可能的回文中心，包括单个字符和两个相邻字符
+        for (int i = 0; i< length; i++) {
+            for (int j=0; j <=1; j++) {
+                int l = i; // 回文左指针
+                int r = i+j; // 回文右指针
+
+                // 扩展回文串
+                while(l >=0 && r < length && s.charAt(l) == s.charAt(r)){
+                    l--;
+                    r++;
+                }
+
+                // 回溯到回文字符串的起始和结束位置
+                l++;
+                r--;
+
+                // 比较并保存最长的回文子串起始位置和长度
+                if (maxLength < r - l + 1) {
+                    maxLength = r - l + 1;
+                    maxStart = l;
+                }
+            }
+        }
+
+        // 根据最长回文子串的起始位置和长度截取子串并返回
+        return s.substring(maxStart, maxStart + maxLength);
+    }
+
+}
---
+++ src/dynamic/leetcode64.java
@@ -0,0 +1,45 @@
+package dynamic;
+
+public class leetcode64 {
+    public int minPathSum(int[][] grid) {
+        return dfs(grid, 0, 0);
+    }
+    private int dfs(int[][] grid, int i, int j){
+        // 当到达目标位置时，直接返回该位置的值
+        if(i == grid.length - 1 && j == grid[0].length - 1){
+            return grid[i][j];
+        }
+        // 当到达最后一行时，只能向右移动
+        if(i == grid.length - 1){
+            return grid[i][j] + dfs(grid, i, j + 1);
+        }
+        // 当到达最后一列时，只能向下移动
+        if(j == grid[0].length - 1){
+            return grid[i][j] + dfs(grid, i + 1, j);
+        }
+        // 在其他位置时，可以选择向下或向右移动，选择路径和最小的方向
+        return grid[i][j] + Math.min(dfs(grid, i + 1, j), dfs(grid, i, j + 1));
+    }
+    public int dp(int[][] grid){
+        // 创建一个dp数组来存储路径信息，额外增加的行和列用于边界条件处理
+        int[][] dp = new int[grid.length+1][grid[0].length+1];
+        dp[0][0] = grid[0][0]; // 起始点的代价设为网格自身的值
+
+        // 初始化第一行和第一列的代价，相当于只考虑水平或垂直方向的移动
+        for(int i = 1; i < grid.length; i++){
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for(int j = 1; j < grid[0].length; j++){
+            dp[0][j] = dp[0][j-1] + grid[0][j];
+        }
+
+        // 动态规划填表，每个单元格的最小代价等于其上方和左方单元格最小代价之和的较小值加上自身的代价
+        for(int i = 1; i < grid.length; i++){
+            for(int j = 1; j < grid[0].length; j++){
+                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+            }
+        }
+        // 返回右下角单元格的最小代价
+        return dp[grid.length-1][grid[0].length-1];
+    }
+}
