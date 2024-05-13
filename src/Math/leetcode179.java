f8e2d7499d3a80f534144480e67769f942bde0e6
judymac <mzyy867734122@163.com>
2024-05-13 17:44:44 +0800

    finish 179

---
+++ src/Math/leetcode179.java
@@ -0,0 +1,21 @@
+package Math;
+
+import java.util.Arrays;
+
+public class leetcode179 {
+    public String largestNumber(int[] nums) {
+        String[] strs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strs[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(strs, (x, y) -> (y + x).compareTo(x + y));
+        if (strs[0].equals("0")) {
+            return "0";
+        }
+        StringBuilder res = new StringBuilder();
+        for (String s : strs) {
+            res.append(s);
+        }
+        return res.toString();
+    }
+}
